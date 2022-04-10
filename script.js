10b7238c8c0bbc1ab880384dac9649388bc6c430
Roma-uuu <92431182+Roma-uuu@users.noreply.github.com>
2022-04-10 20:57:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const swiper = new Swiper('.swiper', {
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
     },
 
     // Navigation arrows
@@ -17,5 +19,45 @@ const swiper = new Swiper('.swiper', {
     // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
+        draggable: true
+    },
+
+    // Управление стрелками с клавы
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+    },
+
+    // Управление колесом мыши
+    mousewheel: {
+        sensitivity: 1,
+        eventsTarget: ".swiper"
+    },
+    // ПОдстройка высоты слайдера для каждого изображения
+    autoHeight: true,
+    // Количество слайдов для показа
+    slidesPerView: 3,
+    // Если слайдов меньше чем в slidesPerview, то свайпер отключится
+    watchOverflow: true,
+
+    //автопрокрутка
+    autoplay: {
+        delay: 2000, //пауза при прокрутке
+        //оставка прокрутки на последнем слайде
+        stopOnLastSlide: false,
+        disableOnInteraction: false //откл. после ручного переключения
+    },
+    speed: 800,
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        }
     },
 });
